53fb728e6c2c6c83c3a58663be6ffc1a5f2835ad
brynsgtn <bryansuguitan17@gmail.com>
2025-08-29 15:43:35 -0500

    Set up vehicle controller connection

--- backend/routes/vehicle.route.js
+++ backend/routes/vehicle.route.js
@@ -1,11 +1,13 @@
+// Import dependencies
 import express from "express";
 
+// Import controllers
+import { viewVehicle } from "../controllers/vehicle.controller.js";
+// Initialize Express router
 const router = express.Router();
 
+// Vehicle routes
+router.get("/", viewVehicle);
 
-router.get("/", (req, res) => {
-    res.send("Vehicle route");
-});
-
-
+// Export router
 export default router;
