176a641ca4ea0df078ff7a7d67a07c88219b98a2
brynsgtn <bryansuguitan17@gmail.com>
2025-08-29 15:13:44 -0500

    Configure routes connection

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,37 @@
+// Import dependencies
 import express from "express";
 import dotenv from "dotenv";
 
+// Import route modules
+import authRoutes from "../backend/routes/auth.route.js";
+import userRoutes from "../backend/routes/user.route.js"
+import vehicleRoutes from "./routes/vehicle.route.js"
+import logRoutes from "./routes/log.route.js"
+
+// Load environment variables from .env
 dotenv.config();
 
+// Initialize Express app
 const app = express();
+
+// Define server port
 const PORT = process.env.PORT || 5001;
 
-app.use("/", (req, res) => {
-    console.log("Welcome to PlateAccess server!")
-    res.send("Welcome to PlateAccess server!");
-});
+// TODO: Set up controllers and database connection.
 
-// TO DO: routes, controllers, and database connection.
+// Routes for authentication
+app.use("/api/auth", authRoutes);
 
+// Routes for user management (admin-only)
+app.use("/api/user", userRoutes);
+
+// Routes for vehicle information - admin(read and write), staff(read-only)
+app.use("/api/vehicle", vehicleRoutes);
+
+// Routes for vehicle logs - admin(read only), staff(read and write)
+app.use("/api/log", logRoutes);
+
+// Start server and listen on defined port
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 });
