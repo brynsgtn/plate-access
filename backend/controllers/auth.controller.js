c8ef2ae942a54f1ed2cf9721673b2efffdac44ec
brynsgtn <bryansuguitan17@gmail.com>
2025-08-29 15:31:23 -0500

    Add comments

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,8 @@
-
+// Login controller
 export const login = async(req, res) => {
     res.send("Login user");
 };
-
+// Logout controller
 export const logout = async(req, res) => {
     res.send("Logout user");
 };
