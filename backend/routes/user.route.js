22507c0ac486f196ac8c33b9c9da7da4c01f509c
brynsgtn <bryansuguitan17@gmail.com>
2025-08-30 15:33:45 -0500

    Change get method to post method on register route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // TODO - add middleware for admin only access
 // User routes
-router.get("/register", registerUser); // Route for user registration (ADMIN ONLY)
+router.post("/register", registerUser); // Route for user registration (ADMIN ONLY)
 router.get("/update", updateUser); // Route for updating user (ADMIN ONLY)
 router.get("/delete", deleteUser); // Route for deleting user (ADMIN ONLY)
 
