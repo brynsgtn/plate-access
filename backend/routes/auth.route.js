00f5baf03da906371a1b55eb781566cb12b98da3
brynsgtn <bryansuguitan17@gmail.com>
2025-08-31 15:16:17 -0500

    Change get method to post method on login route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -8,8 +8,8 @@ import { loginUser, logoutUser } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 // Authentication routes
-router.get("/login", loginUser); // Routes for user log in
-router.get("/logout", logoutUser); // Routes for user log out
+router.post("/login", loginUser); // Route for user log in
+router.get("/logout", logoutUser); // Route for user log out
 
 // Export router
 export default router;
