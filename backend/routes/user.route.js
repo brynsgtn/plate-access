490d0917ead846f8ed025912055bec0f8313840b
brynsgtn <bryansuguitan17@gmail.com>
2025-09-01 14:50:03 -0500

    Implement adminRoute

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,14 +3,17 @@ import express from "express";
 
 // Import controllers
 import { registerUser, updateUser, deleteUser } from "../controllers/user.controller.js";
+
+// Import middleware
+import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
+
 // Initialize Express router
 const router = express.Router();
 
-// TODO - add middleware for admin only access
 // User routes
-router.post("/register", registerUser); // Route for user registration (ADMIN ONLY)
-router.get("/update", updateUser); // Route for updating user (ADMIN ONLY)
-router.get("/delete", deleteUser); // Route for deleting user (ADMIN ONLY)
+router.post("/register", protectRoute, adminRoute, registerUser); // Route for user registration (ADMIN ONLY)
+router.get("/update", protectRoute, adminRoute, updateUser); // Route for updating user (ADMIN ONLY)
+router.get("/delete", protectRoute, adminRoute, deleteUser); // Route for deleting user (ADMIN ONLY)
 
 // Export router
 export default router;
