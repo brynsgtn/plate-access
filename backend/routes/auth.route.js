fbda6b7465923eb16e02eba415c29485f7b458f3
brynsgtn <bryansuguitan17@gmail.com>
2025-09-01 14:18:47 -0500

    Add check-auth route and implement protectRoute middlware and checkAuth controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,12 +2,20 @@
 import express from "express";
 
 // Import controllers
-import { loginUser, logoutUser } from "../controllers/auth.controller.js";
+import {
+    checkAuth,
+    loginUser,
+    logoutUser
+} from "../controllers/auth.controller.js";
+
+// Import middleware
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 // Initialize Express router
 const router = express.Router();
 
 // Authentication routes
+router.get("/check-auth", protectRoute, checkAuth);
 router.post("/login", loginUser); // Route for user log in
 router.post("/logout", logoutUser); // Route for user log out
 
