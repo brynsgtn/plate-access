e71ab043ce8391cf433268582c6f034dabc94de0
brynsgtn <bryansuguitan17@gmail.com>
2025-09-04 21:07:43 -0500

    Add NavBar component

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,246 @@
+import { useEffect } from "react";
+
+const NavBar = () => {
+
+    useEffect(() => {
+        const details = document.querySelectorAll("details");
+
+        // Accordion behavior
+        details.forEach((targetDetail) => {
+            targetDetail.addEventListener("toggle", () => {
+                if (targetDetail.open) {
+                    details.forEach((detail) => {
+                        if (detail !== targetDetail) {
+                            detail.removeAttribute("open");
+                        }
+                    });
+                }
+            });
+        });
+
+        // Close all dropdowns when clicking outside
+        const handleClickOutside = (event) => {
+            details.forEach((detail) => {
+                if (detail.open && !detail.contains(event.target)) {
+                    detail.removeAttribute("open");
+                }
+            });
+        };
+
+        document.addEventListener("click", handleClickOutside);
+
+        // Cleanup
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+    const isAdmin = true;
+
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            {/* Navbar Start (Logo + Mobile Menu) */}
+            <div className="navbar-start">
+                {/* Mobile Dropdown */}
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+
+                        {
+                            isAdmin ? (
+                                // Admin user
+                                <>
+                                    <li >
+                                        <a>System Monitoring</a>
+                                        <ul className="p-2">
+                                            <li><a>Access Logs</a></li>
+                                            <li><a>Alerts</a></li>
+                                            <li><a>Camera Status</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li>
+                                        <a>Vehicle Management</a>
+                                        <ul className="p-2">
+                                            <li><a>View Vehicles</a></li>
+                                            <li><a>Register Vehicle</a></li>
+                                            <li><a>Update Vehicle</a></li>
+                                            <li><a>Delete Vehicle</a></li>
+                                            <li><a>Blacklisted Vehicle</a></li>
+                                            <li><a>Requests</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a>Username</a>
+                                        <ul className="p-2">
+                                            <li><a>Profile</a></li>
+                                            <li><a>Logout</a></li>
+                                        </ul>
+                                    </li>
+                                </>
+                            ) : (
+                                // Non-admin user (Parking staff) 
+                                <>
+
+                                    <li>
+                                        <a>Manual Intervention</a>
+                                        <ul className="p-2">
+                                            <li><a>Manual Plate Entry</a></li>
+                                            <li><a>Gate Controls</a></li>
+                                            <li><a>Add Vehicle</a></li>
+                                        </ul>
+                                    </li>
+                                    <li >
+                                        <a>Monitoring</a>
+                                        <ul className="p-2">
+                                            <li><a>Access Logs</a></li>
+                                            <li><a>Status</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a>Blacklisted Vehicles</a>
+                                        <ul className="p-2">
+                                            <li><a>View Blacklisted Vehicles</a></li>
+                                            <li><a>Add to Blacklist</a></li>
+                                            <li><a>Remove from Blacklist</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a>Alerts</a>
+                                        <ul className="p-2">
+                                            <li><a>Verification Alerts</a></li>
+                                            <li><a>Blacklisted Alert</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a>Username</a>
+                                        <ul className="p-2">
+                                            <li><a>Profile</a></li>
+                                            <li><a>Logout</a></li>
+                                        </ul>
+                                    </li>
+                                </>
+                            )
+                        }
+
+                    </ul>
+                </div>
+                {/* Logo */}
+                <a className="btn btn-ghost text-xl">PlateAccess</a>
+            </div>
+
+            {/* Navbar Center (Desktop Menu) */}
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-4">
+
+                    {isAdmin ? (
+                        // Admin user
+                        <>
+                            <li>
+                                <details>
+                                    <summary>System Monitoring</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>Access Logs</a></li>
+                                        <li><a>Alerts</a></li>
+                                        <li><a>Camera Status</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>Vehicle Management</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>View Vehicles</a></li>
+                                        <li><a>Register Vehicle</a></li>
+                                        <li><a>Update Vehicle</a></li>
+                                        <li><a>Delete Vehicle</a></li>
+                                        <li><a>Blacklisted Vehicle</a></li>
+                                        <li><a>Requests</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                        </>
+
+
+                    ) : (
+                        // Non-admin user (Parking staff) 
+                        <>
+                            <li>
+                                <details>
+                                    <summary>Manual Intervention</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>Manual Plate Entry</a></li>
+                                        <li><a>Gate Controls</a></li>
+                                        <li><a>Add Vehicle</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>Monitoring</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>Access Logs</a></li>
+                                        <li><a>Status</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>Blacklisted Vehicles</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>View Blacklisted Vehicles</a></li>
+                                        <li><a>Add to Blacklist</a></li>
+                                        <li><a>Remove from Blacklist</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>Alerts</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>Verification Alerts</a></li>
+                                        <li><a>Blacklisted Alert</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>
+                                <details>
+                                    <summary>Username</summary>
+                                    <ul className="p-2 bg-base-100">
+                                        <li><a>Profile</a></li>
+                                        <li><a>Logout</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                        </>
+                    )
+                    }
+
+                </ul>
+            </div>
+
+            {/* Navbar End (Button, Profile, etc.) */}
+            <div className="navbar-end hidden lg:flex me-5">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <details>
+                            <summary>Username</summary>
+                            <ul className="bg-base-100 rounded-t-none p-2">
+                                <li><a>Profile</a></li>
+                                <li><a>Logout</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
+        </div >
+
+    )
+}
+
+export default NavBar
