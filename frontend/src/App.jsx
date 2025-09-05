4fc315130a32811d0e09ee55a0f3e36cf8b6c2ca
brynsgtn <bryansuguitan17@gmail.com>
2025-09-04 21:09:31 -0500

    Set up NavBar, HomePage, Footer, and implement theme q

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,33 @@
 // Import necessary components from React Router
-import { Routes , Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 // Import pages
 import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar';
+import Footer from './pages/Footer';
 
 // Main application component
 function App() {
-  
+
   return (
-    // Define the routes for the application
-    <Routes>
-      <Route path="/" element={<HomePage />} /> {/* Home route */}
-    </Routes>
+    <div data-theme="autumn" className="min-h-screen flex flex-col">
+      <NavBar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+
   )
 }
 
 export default App;
+
+
+// TODO: 
+// Navbar component, extract the navbar code from HomePage
+// HomePage
+// Login page, route and component
+// Dashboard page, route and component (Admin and Parking Staff)
