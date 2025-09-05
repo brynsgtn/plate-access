0f892fa30e1c5e8d968e57208c8ba0e7fd8bc20e
brynsgtn <bryansuguitan17@gmail.com>
2025-09-05 16:52:44 -0500

    Set up server proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  publicDir: 'public'
 })
