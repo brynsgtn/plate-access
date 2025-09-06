4ea0afa532c197dec9e5bbf046dc5c511a8581a1
brynsgtn <bryansuguitan17@gmail.com>
2025-09-06 15:42:48 -0500

    Add LoadingSpinner

---
+++ frontend/src/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+
+
+const LoadingSpinner = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen '>
+      <span className="loading loading-bars loading-xl text-primary"></span>
+    </div>
+  )
+}
+
+export default LoadingSpinner
