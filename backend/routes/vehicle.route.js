90e7b6e9975aa5bd41eb0f4cdeba32a1343008ac
brynsgtn <bryansuguitan17@gmail.com>
2025-09-10 15:18:26 -0500

    Add route for blacklist-unblacklist-vehicle

--- backend/routes/vehicle.route.js
+++ backend/routes/vehicle.route.js
@@ -2,7 +2,7 @@
 import express from "express";
 
 // Import controllers
-import { viewVehicles, addVehicle, updateVehicle } from "../controllers/vehicle.controller.js";
+import { viewVehicles, addVehicle, updateVehicle, blackListOrUnblacklistVehicle } from "../controllers/vehicle.controller.js";
 
 // Import middleware
 import { protectRoute } from "../middlewares/auth.middleware.js";
@@ -14,6 +14,7 @@ const router = express.Router();
 router.get("/view-vehicle", protectRoute, viewVehicles);
 router.post("/add-vehicle", protectRoute, addVehicle);
 router.put("/update-vehicle", protectRoute, updateVehicle);
+router.patch("/blacklist-unblacklist-vehicle", protectRoute, blackListOrUnblacklistVehicle);
 
 // Export router
 export default router;
