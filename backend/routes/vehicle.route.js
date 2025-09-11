7f2cbb6b167adf9f1ca96aca44ab099522bdf4c5
brynsgtn <bryansuguitan17@gmail.com>
2025-09-11 15:38:49 -0500

    Add route for delete-vehicle

--- backend/routes/vehicle.route.js
+++ backend/routes/vehicle.route.js
@@ -10,7 +10,8 @@ import {
     viewVehicleRequests, 
     approveVehicleRequest, 
     viewBlacklistedVehicles, 
-    requestUpdateVehicle 
+    requestUpdateVehicle,
+    deleteVehicle,
 } from "../controllers/vehicle.controller.js";
 
 // Import middleware
@@ -24,10 +25,11 @@ router.get("/view-vehicle", protectRoute, viewVehicles);
 router.get("/view-add-vehicle-requests", protectRoute, adminRoute, viewVehicleRequests);
 router.get("/view-blacklisted-vehicles", protectRoute, adminRoute, viewBlacklistedVehicles);
 router.post("/add-vehicle", protectRoute, addVehicle);
-router.put("/update-vehicle", protectRoute, updateVehicle);
+router.put("/update-vehicle", protectRoute, adminRoute, updateVehicle);
 router.put("/request-update-vehicle", protectRoute, requestUpdateVehicle);
 router.patch("/blacklist-unblacklist-vehicle", protectRoute, blackListOrUnblacklistVehicle);
 router.patch("/approve-add-vehicle-request", protectRoute, adminRoute, approveVehicleRequest);
+router.delete("/delete-vehicle", protectRoute, adminRoute, deleteVehicle);
 
 // Export router
 export default router;
