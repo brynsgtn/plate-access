cabcb88ef658d2f684fb6a3d5f91c6a7e7cac296
brynsgtn <bryansuguitan17@gmail.com>
2025-09-11 15:26:00 -0500

    Add routes for request-update-vehicle

--- backend/routes/vehicle.route.js
+++ backend/routes/vehicle.route.js
@@ -2,7 +2,16 @@
 import express from "express";
 
 // Import controllers
-import { viewVehicles, addVehicle, updateVehicle, blackListOrUnblacklistVehicle, viewVehicleRequests, approveVehicleRequest, viewBlacklistedVehicles } from "../controllers/vehicle.controller.js";
+import { 
+    viewVehicles, 
+    addVehicle, 
+    updateVehicle, 
+    blackListOrUnblacklistVehicle, 
+    viewVehicleRequests, 
+    approveVehicleRequest, 
+    viewBlacklistedVehicles, 
+    requestUpdateVehicle 
+} from "../controllers/vehicle.controller.js";
 
 // Import middleware
 import { protectRoute, adminRoute } from "../middlewares/auth.middleware.js";
@@ -16,6 +25,7 @@ router.get("/view-add-vehicle-requests", protectRoute, adminRoute, viewVehicleRe
 router.get("/view-blacklisted-vehicles", protectRoute, adminRoute, viewBlacklistedVehicles);
 router.post("/add-vehicle", protectRoute, addVehicle);
 router.put("/update-vehicle", protectRoute, updateVehicle);
+router.put("/request-update-vehicle", protectRoute, requestUpdateVehicle);
 router.patch("/blacklist-unblacklist-vehicle", protectRoute, blackListOrUnblacklistVehicle);
 router.patch("/approve-add-vehicle-request", protectRoute, adminRoute, approveVehicleRequest);
 
