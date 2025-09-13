4abce4f1c738a432eb05a97dd817d081c257df5f
brynsgtn <bryansuguitan17@gmail.com>
2025-09-13 15:03:53 -0500

    Add viewVehicle function

--- frontend/src/stores/useVehicleStore.js
+++ frontend/src/stores/useVehicleStore.js
@@ -5,6 +5,8 @@ import toast from "react-hot-toast";
 
 export const useVehicleStore = create((set, get) => ({
     vehicles: [],
+    totalVehicles: 0,
+    loading: false,
     addLoading: false,
 
     addVehicle: async (vehicleData) => {
@@ -12,7 +14,8 @@ export const useVehicleStore = create((set, get) => ({
         try {
             const response = await axios.post("/vehicle/add-vehicle", vehicleData);
             set((prevState) => ({
-                vehicles: [...prevState.vehicles, response.data],
+                vehicles: [...prevState.vehicles, response.data.vehicle],
+                totalVehicles: prevState.totalVehicles + 1,
                 addLoading: false
             }));
             toast.success(response?.data?.message || "Vehicle added successfully!");
@@ -20,6 +23,17 @@ export const useVehicleStore = create((set, get) => ({
             console.error("Error adding vehicle:", error);
             set({ addLoading: false });
             toast.error(error.response?.data?.message || "Failed to add vehicle.");
+        }
+    },
+    viewVehicles: async () => {
+        set({ loading: true });
+        try {
+            const response = await axios.get("/vehicle/view-vehicle");
+            console.log("Vehicles:", response.data.vehicles);
+            set({ vehicles: response.data.vehicles, totalVehicles: response.data.totalVehicles,loading: false });
+        } catch (error) {
+            console.error("Error viewing vehicles:", error);
+            set({ loading: false });
         }
     }
 }));
