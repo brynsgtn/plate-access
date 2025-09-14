cece48775e490320dd8607348c16a217845f2d84
brynsgtn <bryansuguitan17@gmail.com>
2025-09-14 15:35:06 -0500

    Add search bar feature

--- frontend/src/components/BlacklistedVehicleList.jsx
+++ frontend/src/components/BlacklistedVehicleList.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useVehicleStore } from "../stores/useVehicleStore";
+import { Search } from "lucide-react";
 
 const VEHICLES_PER_PAGE = 2;
 
 const VehicleBlacklistTable = () => {
 
     const [page, setPage] = useState(1);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        console.log("Search Data:", searchTerm);
+    }, [searchTerm]);
 
     const { vehicles, blacklistOrUnblacklistVehicle } = useVehicleStore();
     const blacklistedVehicles = vehicles.filter((vehicle) => vehicle.isBlacklisted);
 
-    const totalPages = blacklistedVehicles ? Math.ceil(blacklistedVehicles.length / VEHICLES_PER_PAGE) : 1;
-    const paginatedVehicles = blacklistedVehicles
-        ? blacklistedVehicles.slice((page - 1) * VEHICLES_PER_PAGE, page * VEHICLES_PER_PAGE)
+     // Filter vehicles by plate number
+    const filteredVehicles = blacklistedVehicles.filter((vehicle) =>
+        vehicle.plateNumber.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+
+    const totalPages = filteredVehicles ? Math.ceil(filteredVehicles.length / VEHICLES_PER_PAGE) : 1;
+    const paginatedVehicles = filteredVehicles
+        ? filteredVehicles.slice((page - 1) * VEHICLES_PER_PAGE, page * VEHICLES_PER_PAGE)
         : [];
 
     const handleUnblacklist = (id) => {
@@ -24,6 +36,24 @@ const VehicleBlacklistTable = () => {
         <>
 
             <div className="overflow-x-auto max-w-6xl mx-auto my-10 rounded-xl shadow-lg bg-base-100 border border-base-300">
+                {/* Header with Search */}
+                <div className="flex flex-col md:flex-row justify-between items-center gap-4 p-4 border-b border-base-300">
+                    <h2 className="text-xl font-bold text-primary">Vehicle List</h2>
+
+                    <div className="flex flex-col md:flex-row gap-3 w-full md:w-auto">
+                        {/* Search Bar */}
+                        <label className="input flex items-center gap-2 w-full md:w-64">
+                            <Search className="w-4 h-4 opacity-70" />
+                            <input
+                                type="text"
+                                className="grow"
+                                placeholder="Search by plate number"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                </div>
                 <table className="table table-zebra w-full">
                     <thead className="bg-base-200">
                         <tr>
