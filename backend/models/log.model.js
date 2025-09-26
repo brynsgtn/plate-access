9c21732e6d1ceb47ae7108b949c57e1a3e6eb7e9
brynsgtn <bryansuguitan17@gmail.com>
2025-09-26 17:20:35 -0500

    Add blacklistHit field

--- backend/models/log.model.js
+++ backend/models/log.model.js
@@ -1,37 +1,41 @@
 import mongoose from "mongoose";
 
 const logSchema = new mongoose.Schema({
-  vehicle: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Vehicle",
-    required: false // in case the plate is not registered
-  },
-  plateNumber: {
-    type: String,
-    required: true // always log the attempted plate, even if not in DB
-  },
-  gateType: {
-    type: String,
-    enum: ["entrance", "exit"],
-    required: true
-  },
-  method: {
-    type: String,
-    enum: ["LPR", "manual"],
-    required: true
-  },
-  success: {
-    type: Boolean,
-    required: true
-  },
-  notes: {
-    type: String,
-    default: ""
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
+    vehicle: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vehicle",
+        required: false // in case the plate is not registered
+    },
+    plateNumber: {
+        type: String,
+        required: true // always log the attempted plate, even if not in DB
+    },
+    gateType: {
+        type: String,
+        enum: ["entrance", "exit"],
+        required: true
+    },
+    method: {
+        type: String,
+        enum: ["LPR", "manual"],
+        required: true
+    },
+    success: {
+        type: Boolean,
+        required: true
+    },
+    blacklistHit: {
+        type: Boolean,
+        default: false // becomes true if plate is found in blacklist
+    },
+    notes: {
+        type: String,
+        default: ""
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Log = mongoose.model("Log", logSchema);
