704bfd878ea77848ce44aff49fc48e29b299a3eb
brynsgtn <bryansuguitan17@gmail.com>
2025-09-26 12:47:51 -0500

    Add lastEntranceAction and lastExitAction state

--- frontend/src/stores/useGateStore.js
+++ frontend/src/stores/useGateStore.js
@@ -1,6 +1,5 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from 'zustand/middleware'
-
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export const useGateStore = create()(
   persist(
@@ -8,10 +7,27 @@ export const useGateStore = create()(
       // Initial states
       isEntranceGateOpen: false,
       isExitGateOpen: false,
+      lastEntranceAction: null, // { action: "open" | "close", time: "12:30 PM" }
+      lastExitAction: null,     // { action: "open" | "close", time: "12:30 PM" }
 
       // Actions
-      setIsEntranceGateOpen: (value) => set({ isEntranceGateOpen: value }),
-      setIsExitGateOpen: (value) => set({ isExitGateOpen: value }),
+      setIsEntranceGateOpen: (value) =>
+        set({
+          isEntranceGateOpen: value,
+          lastEntranceAction: {
+            action: value ? "opened" : "closed",
+            time: new Date().toLocaleTimeString(),
+          },
+        }),
+
+      setIsExitGateOpen: (value) =>
+        set({
+          isExitGateOpen: value,
+          lastExitAction: {
+            action: value ? "opened" : "closed",
+            time: new Date().toLocaleTimeString(),
+          },
+        }),
     }),
     {
       name: "gate-store", // key in localStorage
