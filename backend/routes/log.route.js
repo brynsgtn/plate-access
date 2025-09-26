f57589842c5626378ec2c23b34367c7a2372b378
brynsgtn <bryansuguitan17@gmail.com>
2025-09-26 17:04:21 -0500

    Add route for exitLogLPR controller

--- backend/routes/log.route.js
+++ backend/routes/log.route.js
@@ -4,7 +4,8 @@ import express from "express";
 // Import controllers
 import { 
     viewAllLogs, 
-    entryLogLPR
+    entryLogLPR,
+    exitLogLPR
  } from "../controllers/log.controller.js";
 
 // Initialize Express router
@@ -13,6 +14,7 @@ const router = express.Router();
 // Log routes
 router.get("/view-logs", viewAllLogs);
 router.post("/entry-log-lpr", entryLogLPR);
+router.post("/exit-log-lpr", exitLogLPR);
 
 // Export router
 export default router;
