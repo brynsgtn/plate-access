f15087ef6ea52e9bb3ccb52d4bdc22fcd8f336dd
brynsgtn <bryansuguitan17@gmail.com>
2025-09-26 12:46:38 -0500

    Add route for entryLog function

--- backend/routes/log.route.js
+++ backend/routes/log.route.js
@@ -2,13 +2,17 @@
 import express from "express";
 
 // Import controllers
-import { viewLogs } from "../controllers/log.controller.js";
+import { 
+    viewLogs, 
+    entryLog
+ } from "../controllers/log.controller.js";
 
 // Initialize Express router
 const router = express.Router();
 
 // Log routes
 router.get("/", viewLogs);
+router.post("/log-entry", entryLog);
 
 // Export router
 export default router;
