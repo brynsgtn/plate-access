d6c201c2ba0d79494b245c6bd869742443886ada
brynsgtn <bryansuguitan17@gmail.com>
2025-09-26 17:04:00 -0500

    Add exitLogLPR controller

--- backend/controllers/log.controller.js
+++ backend/controllers/log.controller.js
@@ -75,13 +75,13 @@ export const entryLogLPR = async (req, res) => {
 
 
     } catch (error) {
-        console.error("Error in entryLog controller:", error);
+        console.error("Error in entryLogLPR controller:", error);
         res.status(500).json({ message: "Server error" });
     }
 }
 
 // Exit log controller
-export const exitLog = async (req, res) => {
+export const exitLogLPR = async (req, res) => {
     const { plateNumber } = req.body;
 
     try {
@@ -89,27 +89,36 @@ export const exitLog = async (req, res) => {
             return res.status(400).json({ message: "Plate number is required" });
         }
 
+        // First, check if it’s a registered vehicle
         const vehicle = await Vehicle.findOne({ plateNumber });
 
-        if (!vehicle) {
-            return res.status(404).json({ message: "Vehicle not found" });
+        if (vehicle) {
+
+
+            // Case 1: Success
+            const log = await Log.create({
+                vehicle: vehicle._id,
+                plateNumber,
+                gateType: "exit",
+                method: "LPR",
+                success: true,
+                notes: "Verified by LPR"
+            });
+            return res.status(201).json({ message: "Exit granted", log });
         }
 
-
-        const exitLog = await Log.create({
-            vehicle: vehicle._id,
+        // Case 2: Unrecognized Plate
+        const log = await Log.create({
+            plateNumber,
             gateType: "exit",
-            action: "open",
-            success: true,
             method: "LPR",
-            timestamp: new Date(),
-            notes: "Verified by LPR",
+            success: false,
+            notes: "Unrecognized Plate Number"
         });
-
-        res.status(201).json(exitLog);
+        return res.status(403).json({ message: "Unrecognized Plate Number, please enter plate number manually", log });
 
     } catch (error) {
-        console.error("Error in exitLog controller:", error);
+        console.error("Error in exitLogLPR controller:", error);
         res.status(500).json({ message: "Server error" });
     }
-}
+};
