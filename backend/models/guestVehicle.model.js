501c6767537d9241817bb78d106d40c2437592e2
brynsgtn <bryansuguitan17@gmail.com>
2025-09-27 22:14:49 -0500

    Add guestVehicle model

---
+++ backend/models/guestVehicle.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const guestVehicleSchema = new mongoose.Schema(
+    {
+        plateNumber: {
+            type: String,
+            required: true,
+        },
+        makeModel: {
+            type: String,
+            required: true,
+        },
+        ownerName: {
+            type: String,
+            required: true,
+        },
+        addedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User", // the staff/admin who authorized
+            required: true,
+        },
+        validFrom: {
+            type: Date,
+            required: true,
+            default: Date.now, // can start immediately
+        },
+        validUntil: {
+            type: Date,
+            required: true, // e.g., valid for 1 day
+        },
+        notes: {
+            type: String,
+            default: "",
+        },
+        isBlacklisted: {
+            type: Boolean,
+            default: false,
+        },
+        isBlacklistedAt: {
+            type: Date,
+            default: null,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const GuestVehicle = mongoose.model("GuestVehicle", guestVehicleSchema);
+
+export default GuestVehicle;
