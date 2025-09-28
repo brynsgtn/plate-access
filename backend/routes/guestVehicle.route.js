6f762b63101a94dee3b714e0cfe3efc944e0a03e
brynsgtn <bryansuguitan17@gmail.com>
2025-09-27 22:52:37 -0500

    Add route for extendGuestVehicle

--- backend/routes/guestVehicle.route.js
+++ backend/routes/guestVehicle.route.js
@@ -6,6 +6,7 @@ import express from "express";
 import {
     viewGuestVehicles,
     addGuestVehicle,
+    extendGuestVehicle
 } from "../controllers/guestVehicle.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
@@ -15,6 +16,7 @@ const router = express.Router();
 // Guest vehicle routes
 router.get("/view-guest-vehicles", protectRoute, viewGuestVehicles);
 router.post("/add-guest-vehicle", protectRoute, addGuestVehicle);
+router.patch("/extend-guest-vehicle", protectRoute, extendGuestVehicle);
 
 // Export router
 export default router;
