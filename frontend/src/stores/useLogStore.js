2b2057b5f56c2e053ef74e55860395ec37ce1eac
brynsgtn <bryansuguitan17@gmail.com>
2025-10-01 16:40:56 -0500

    Add lprEntryLogAttempt function

--- frontend/src/stores/useLogStore.js
+++ frontend/src/stores/useLogStore.js
@@ -30,5 +30,20 @@ export const useLogStore = create((set) => ({
             toast.error(error.response?.data?.message || "Failed to create log.");
             return { success: false, error: error.response?.data }; // return failure
         }
-    }
+    },
+
+    manualExitLogAttempt: async ({ plateNumber }) => {
+        set({ loading: true });
+        try {
+            const response = await axios.post("/log/exit-log-manual", { plateNumber });
+            set({ loading: false });
+            toast.success(response.data.message || "Log created successfully!");
+            return { success: true, data: response.data }; // return result
+        } catch (error) {
+            set({ loading: false });
+            console.error("Create log failed:", error);
+            toast.error(error.response?.data?.message || "Failed to create log.");
+            return { success: false, error: error.response?.data }; // return failure
+        }
+    },
 }))
