8e7627031bd1e32cf673f45184cb352345ee2167
brynsgtn <bryansuguitan17@gmail.com>
2025-10-01 16:17:53 -0500

    Add manualEntryLogAttempt function

--- frontend/src/stores/useLogStore.js
+++ frontend/src/stores/useLogStore.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
 
-export const useLogStore = create ((set) => ({
+export const useLogStore = create((set) => ({
     logs: [],
     loading: false,
 
@@ -13,6 +14,21 @@ export const useLogStore = create ((set) => ({
         } catch (error) {
             console.error("Error fetching logs:", error);
             set({ loading: false });
+        }
+    },
+
+    manualEntryLogAttempt: async ({ plateNumber }) => {
+        set({ loading: true });
+        try {
+            const response = await axios.post("/log/entry-log-manual", { plateNumber });
+            set({ loading: false });
+            toast.success(response.data.message || "Log created successfully!");
+            return { success: true, data: response.data }; // return result
+        } catch (error) {
+            set({ loading: false });
+            console.error("Create log failed:", error);
+            toast.error(error.response?.data?.message || "Failed to create log.");
+            return { success: false, error: error.response?.data }; // return failure
         }
     }
 }))
