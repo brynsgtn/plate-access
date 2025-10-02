cc1e4e117b12e26e0fd60b9a1e5e51a24dc73a3a
brynsgtn <bryansuguitan17@gmail.com>
2025-10-02 15:33:53 -0500

    Add comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -75,9 +75,11 @@ export default App;
 // TODO: 
 
 // PARKING STAFF
-// Dashboard page - build backend and integrate with frontend (camera, access logs, verification alerts)
-// Access control page - build backend and integrate with frontend (gate controls - automatic lpr attempt on entrance and exit, and manual controls - gate, camera entry,)
+// Dashboard page - build backend and integrate with frontend (camera, verification alerts, last activity section)
+// Access control page - build backend and integrate with frontend (simulation/integration of automatic lpr attempt on entrance and exit, make last activity a global state and display to dashboard page)
 // Vehicle Management page - summary (check sub stats)
+// Access Log page - implement who created the log, implement protect middleware on routes
+
 
 // ADMIN
 // Dashboard page 
