52a823b7aea248aa541f83c7b11708b1b28555da
brynsgtn <bryansuguitan17@gmail.com>
2025-10-14 22:31:53 -0500

    Fix deployment issue

--- backend/server.js
+++ backend/server.js
@@ -46,10 +46,10 @@ app.use("/api/log", logRoutes);
 
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
 
